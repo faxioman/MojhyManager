23eeb41a80393522fdcfe1ba567d1bc3627d3026
fabio <fabio@10b469d9-a807-0410-b80a-b41ed0d37aed>
2006-01-17 18:40:56 +0000

    git-svn-id: file:///Users/fabio/Desktop/svnrepos/MojhyManager@66 10b469d9-a807-0410-b80a-b41ed0d37aed

--- WebProject/WinTest/SoccerTest.cs
+++ WebProject/WinTest/SoccerTest.cs
@@ -53,7 +53,7 @@ namespace WinTest
         {
             l_ptMouseLoc.X = e.X;
             l_ptMouseLoc.Y = e.Y;
-            Invalidate(new Rectangle(0,0,200,20));
+            Invalidate(new Rectangle(0,0,120,12));
         }
         //click sul campo
         void SoccerTest_Click(object sender, EventArgs e)
@@ -87,10 +87,15 @@ namespace WinTest
             l_objSoccerGraph.RenderField(pe.Graphics, blShowAreas);
             //calcolo il punto del mouse in mm
             Point ptMouseLocMM = l_objSoccerGraph.PixelToMM(pe.Graphics, l_ptMouseLoc);
+            if ((ptMouseLocMM.X > l_objField.Width) || (ptMouseLocMM.Y > l_objField.Height))
+            {
+                ptMouseLocMM.X = 0;
+                ptMouseLocMM.Y = 0;
+            }
             //attivo l'area cliccata
             if (l_blAreaSelected) l_objSoccerGraph.DrawSelectedArea(pe.Graphics, l_ptBall);
             //scrivo la posizione del mouse
-            pe.Graphics.DrawString("X: " + ptMouseLocMM.X.ToString() + "mm  Y: " + ptMouseLocMM.Y.ToString() + "mm", new Font("Arial", 12), Brushes.White, new PointF(100, 100));
+            pe.Graphics.DrawString("X: " + ptMouseLocMM.X.ToString() + "mm  Y: " + ptMouseLocMM.Y.ToString() + "mm", new Font("Tahoma", 7), Brushes.White, new PointF(100, 100));
         }
         //click per visualizzare o nascondere le aree del campo
         private void btShowAreas_Click(object sender, EventArgs e)
