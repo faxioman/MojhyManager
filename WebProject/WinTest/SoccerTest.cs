8d046fce1982d21e2cc110f016f11ba584639f08
fabio <fabio@10b469d9-a807-0410-b80a-b41ed0d37aed>
2006-01-22 13:39:45 +0000

    git-svn-id: file:///Users/fabio/Desktop/svnrepos/MojhyManager@73 10b469d9-a807-0410-b80a-b41ed0d37aed

--- WebProject/WinTest/SoccerTest.cs
+++ WebProject/WinTest/SoccerTest.cs
@@ -11,6 +11,10 @@ namespace WinTest
 {
     public partial class SoccerTest : Form
     {
+        private const int MINX = 8;
+        private const int MAXX = 786;
+        private const int MINY = 8;
+        private const int MAXY = 520;
         private Mojhy.Engine.Field l_objField;
         private SoccerGraphics l_objSoccerGraph;
         //squadre
@@ -34,6 +38,8 @@ namespace WinTest
         private bool l_blShowDefense = true;
         //definisco se sono in fase di drag
         private bool l_blDragging = false;
+        //area di validità di spostamento dei giocatori
+        private Rectangle l_rctValidPosition;
         //l'enumeratore FormStatus indica lo stato corrente dell'applicativo
         public enum FormStatus
         {
@@ -64,8 +70,20 @@ namespace WinTest
             {
                 objPlayingPlayer.PositionOnField = new Point(l_objField.Width / 2, l_objField.Height / 2);
             }
+            //inizializzo l'area di validità per lo spostamento manuale dei giocatori
+            //l_rctValidPosition = new Rectangle(
         }
-
+        //restituisce un punto valido del campo in pixel
+        private Point GetValidFieldPosition(Point ptRawPoint)
+        {
+            Point ptValidPosition = new Point(ptRawPoint.X,ptRawPoint.Y);
+            if (ptValidPosition.X < MINX) ptValidPosition.X = MINX;
+            if (ptValidPosition.X > MAXX) ptValidPosition.X = MAXX;
+            if (ptValidPosition.Y < MINY) ptValidPosition.Y = MINY;
+            if (ptValidPosition.Y > MAXY) ptValidPosition.Y = MAXY;
+            return ptValidPosition;
+        }
+        //eventu sul rilascio del mouse
         void SoccerTest_MouseUp(object sender, MouseEventArgs e)
         {
             l_blDragging = false;
@@ -77,6 +95,10 @@ namespace WinTest
                     l_ptBall = l_ptMouseLoc;
                     l_blAreaSelected = true;
                     Invalidate();
+                }
+                else
+                {
+                    Cursor.Show();
                 }
             }
             //deseleziono qualsiasi eventuale giocatore selezionato
@@ -93,11 +115,12 @@ namespace WinTest
                 //calcolo il rettangolo che definisce il range di selezione del giocatore
                 Point ptMousePositionInField = l_objSoccerGraph.PixelToMM(l_ptMouseLoc);
                 Rectangle rctSelectionArea = new Rectangle(ptMousePositionInField.X - 1500, ptMousePositionInField.Y - 1500, 3000, 3000);
-                foreach (Mojhy.Engine.PlayingPlayer objPlayingPlayer in l_objTeamA.PlayingPlayers)
+                for (int i = l_objTeamA.PlayingPlayers.Length-1; i >= 0; i--)
                 {
-                    if (rctSelectionArea.Contains(objPlayingPlayer.PositionOnField))
+                    if (rctSelectionArea.Contains(l_objTeamA.PlayingPlayers[i].PositionOnField))
                     {
-                        l_intSelectedPlayerIndex = objPlayingPlayer.Index;
+                        l_intSelectedPlayerIndex = l_objTeamA.PlayingPlayers[i].Index;
+                        Cursor.Hide();
                         break;
                     }
                 }
@@ -113,16 +136,16 @@ namespace WinTest
                 //verifico se devo spostare un giocatore
                 if ((l_blDragging) && (l_intSelectedPlayerIndex != -1))
                 {
-
                     Point ptOldPosition = l_objSoccerGraph.MMToPixel(l_objTeamA.PlayingPlayers[l_intSelectedPlayerIndex].PositionOnField);
                     Point ptNewPosition = l_ptMouseLoc;
+                    ptNewPosition = GetValidFieldPosition(ptNewPosition);
                     l_objTeamA.PlayingPlayers[l_intSelectedPlayerIndex].PositionOnField = l_objSoccerGraph.PixelToMM(ptNewPosition);
-                    Invalidate(new Rectangle(ptOldPosition.X - 20, ptOldPosition.Y -20, 40, 40));
+                    Invalidate(new Rectangle(ptOldPosition.X - 20, ptOldPosition.Y - 20, 40, 40));
                     Invalidate(new Rectangle(ptNewPosition.X - 20, ptNewPosition.Y - 20, 40, 40));
                 }
             }
             //invalido l'area dove scrivere la posizione del mouse
-            Invalidate(new Rectangle(0,0,120,12));
+            Invalidate(new Rectangle(0,0,120,25));
 
         }
         //evento di ridisegno della form
@@ -144,10 +167,13 @@ namespace WinTest
             l_objSoccerGraph.RenderField(l_objField, blShowAreas);
             //calcolo il punto del mouse in mm
             Point ptMouseLocMM = l_objSoccerGraph.PixelToMM(l_ptMouseLoc);
-            if ((ptMouseLocMM.X > l_objField.Width) || (ptMouseLocMM.Y > l_objField.Height))
+            Point ptMouseLocTmp = l_ptMouseLoc;
+            if ((ptMouseLocMM.X > l_objField.Width) || (ptMouseLocMM.Y > l_objField.Height) || (ptMouseLocMM.X < 0) || (ptMouseLocMM.Y < 0))
             {
                 ptMouseLocMM.X = 0;
                 ptMouseLocMM.Y = 0;
+                ptMouseLocTmp.X = 0;
+                ptMouseLocTmp.Y = 0;
             }
             //attivo l'area cliccata
             if (l_blAreaSelected) l_objSoccerGraph.DrawSelectedArea(l_ptBall, l_objField);
@@ -156,8 +182,9 @@ namespace WinTest
             {
                 l_objSoccerGraph.DrawPlayer(objPlayingPlayer, Mojhy.Engine.PlayingPlayer.PlayerPositionType.Defense);
             }
-            //scrivo la posizione del mouse
-            pe.Graphics.DrawString("X: " + ptMouseLocMM.X.ToString() + "mm  Y: " + ptMouseLocMM.Y.ToString() + "mm", new Font("Tahoma", 7), Brushes.White, new PointF(100, 100));
+            //scrivo la posizione del mouse (in MM e in Pixel)
+           // pe.Graphics.DrawString("X: " + ptMouseLocMM.X.ToString() + "mm  Y: " + ptMouseLocMM.Y.ToString() + "mm", new Font("Tahoma", 7), Brushes.White, new PointF(100, 100));
+           // pe.Graphics.DrawString("X: " + ptMouseLocTmp.X.ToString() + "px  Y: " + ptMouseLocTmp.Y.ToString() + "px", new Font("Tahoma", 7), Brushes.White, new PointF(100, 1600));
         }
         //click per visualizzare o nascondere le aree del campo
         private void btShowAreas_Click(object sender, EventArgs e)
