574820aa7177b34b43cd86d8b1034a2ec5dd06b0
marco <marco@10b469d9-a807-0410-b80a-b41ed0d37aed>
2006-01-29 20:29:33 +0000

    git-svn-id: file:///Users/fabio/Desktop/svnrepos/MojhyManager@87 10b469d9-a807-0410-b80a-b41ed0d37aed

--- WebProject/Mojhy/App_Code/NewFolder1/Division.cs
+++ WebProject/Mojhy/App_Code/NewFolder1/Division.cs
@@ -1,7 +1,7 @@
 /* Division.cs, MARCO CECCARELLI
  * La classe definisce le divisioni (le serie) presenti all'interno di ogni lega */
 
-namespace Mojhy.Calendar
+namespace Mojhy.League
 {
     /// <summary>
     /// Definisce le divisioni
@@ -10,6 +10,7 @@ namespace Mojhy.Calendar
     {
         private string l_strName;
         private int l_intLeagueID;
+        private int l_intDivisionID;
         
         /// <summary>
         /// Gets / Sets the Name of the Divisions.
@@ -28,6 +29,16 @@ namespace Mojhy.Calendar
         {
             get { return l_intLeagueID; }
             set { l_intLeagueID = value; }
+        }
+
+        /// <summary>
+        /// Gets / Sets the DivisionID.
+        /// </summary>
+        /// <value>The Division ID.</value>
+        public int DivisionID
+        {
+            get { return l_intDivisionID; }
+            set { l_intDivisionID = value; }
         }
 
 
--- WebProject/Mojhy/App_Code/NewFolder1/League.cs
+++ WebProject/Mojhy/App_Code/NewFolder1/League.cs
@@ -1,14 +1,15 @@
 /* League.cs, MARCO CECCARELLI
  * La classe definisce le leghe in cui sono presenti i diversi campionati */
 
-namespace Mojhy.Calendar
+namespace Mojhy.League
 {
     /// <summary>
     /// Defines all the twenty areas of the field
     /// </summary>
     class League
     {
-        private string l_strName;       
+        private string l_strName;
+        private int l_intLeagueID;
         
         /// <summary>
         /// Gets the Name of the League.
@@ -17,7 +18,17 @@ namespace Mojhy.Calendar
         {
             get { return l_strName; }
             set { l_strName = value; }
-        }        
+        }
+
+        /// <summary>
+        /// Gets or sets the league ID.
+        /// </summary>
+        /// <value>The league ID.</value>
+        public int LeagueID
+        {
+            get { return l_intLeagueID; }
+            set { l_intLeagueID = value; }
+        }
         
     }
 }
