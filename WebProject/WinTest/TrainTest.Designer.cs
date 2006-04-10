32d2c2672d0c07eee0f574170ff1a4ee77189be9
daniele <daniele@10b469d9-a807-0410-b80a-b41ed0d37aed>
2006-04-10 20:11:36 +0000

    git-svn-id: file:///Users/fabio/Desktop/svnrepos/MojhyManager@160 10b469d9-a807-0410-b80a-b41ed0d37aed

--- WebProject/WinTest/TrainTest.Designer.cs
+++ WebProject/WinTest/TrainTest.Designer.cs
@@ -28,19 +28,44 @@ namespace WinTest
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbProva = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(276, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbProva
+            // 
+            this.tbProva.Location = new System.Drawing.Point(77, 33);
+            this.tbProva.Name = "tbProva";
+            this.tbProva.Size = new System.Drawing.Size(100, 20);
+            this.tbProva.TabIndex = 1;
             // 
             // TrainTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(369, 264);
+            this.Controls.Add(this.tbProva);
+            this.Controls.Add(this.button1);
             this.Name = "TrainTest";
             this.Text = "TrainTest";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbProva;
     }
 }
--- WebProject/WinTest/TrainTest.cs
+++ WebProject/WinTest/TrainTest.cs
@@ -14,5 +14,10 @@ namespace WinTest
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tbProva.Text = "ciao";
+        }
     }
 }
