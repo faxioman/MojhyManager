700debefe5606caadcf1f3e142d2d75850a1bcf8
fabio <fabio@10b469d9-a807-0410-b80a-b41ed0d37aed>
2006-05-13 16:51:11 +0000

    git-svn-id: file:///Users/fabio/Desktop/svnrepos/MojhyManager@169 10b469d9-a807-0410-b80a-b41ed0d37aed

--- WebProject/WinTest/Utils/DrawingExt.cs
+++ WebProject/WinTest/Utils/DrawingExt.cs
@@ -2,34 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Mojhy.Utils.DrawingsExt
+namespace Mojhy.Utils.DrawingExt
 {
+    /// <summary>
+    /// 3D PointF class
+    /// </summary>
     public struct Point3DF
     {
-        Mamanze.Math.Matrix
-          matrix;
-
-        #region public Point3DF(System.Single X, System.Single Y, System.Single Z) : this()
+        Matrix  matrix;
         public Point3DF(System.Single X, System.Single Y, System.Single Z)
         {
-            matrix = new Mamanze.Math.Matrix(1, 4);
+            matrix = new Matrix(1, 4);
             matrix[0, 0] = X;
             matrix[0, 1] = Y;
             matrix[0, 2] = Z;
             matrix[0, 3] = 1;
         }
-        #endregion
-
-        #region public Point3DF(Mamanze.Math.Matrix Matrix)
-        public Point3DF(Mamanze.Math.Matrix Matrix)
+        public Point3DF(Matrix Matrix)
         {
             if (Matrix.Rows != 1 || Matrix.Columns != 4)
                 throw new System.Exception("Matrix must be a 1 by 4 matrix.");
             matrix = Matrix;
         }
-        #endregion
-
-        #region public System.Single X
         public System.Single X
         {
             get
@@ -41,9 +35,6 @@ namespace Mojhy.Utils.DrawingsExt
                 matrix[0, 0] = value;
             }
         }
-        #endregion
-
-        #region public System.Single Y
         public System.Single Y
         {
             get
@@ -55,9 +46,6 @@ namespace Mojhy.Utils.DrawingsExt
                 matrix[0, 1] = value;
             }
         }
-        #endregion
-
-        #region public System.Single Z
         public System.Single Z
         {
             get
@@ -69,24 +57,83 @@ namespace Mojhy.Utils.DrawingsExt
                 matrix[0, 2] = value;
             }
         }
-        #endregion
 
-        #region public Mamanze.Math.Matrix Matrix
-        public Mamanze.Math.Matrix Matrix
+        public Matrix Matrix
         {
             get
             {
                 return matrix;
             }
         }
-        #endregion
-
-        #region public override System.String ToString()
         public override System.String ToString()
         {
             return System.String.Format("({0},{1},{2})", X, Y, Z);
         }
-        #endregion
+    }
+    /// <summary>
+    /// Extended Matrix
+    /// </summary>
+    public class Matrix
+    {
+        System.Double[,] elements;
 
+        public Matrix(System.Int32 Rows, System.Int32 Columns)
+        {
+            elements = new System.Double[Rows, Columns];
+        }
+        public System.Double this[System.Int32 Row, System.Int32 Column]
+        {
+            get
+            {
+                return elements[Row, Column];
+            }
+            set
+            {
+                elements[Row, Column] = value;
+            }
+        }
+        public System.Int32 Rows
+        {
+            get
+            {
+                return elements.GetLength(0);
+            }
+        }
+        public System.Int32 Columns
+        {
+            get
+            {
+                return elements.GetLength(1);
+            }
+        }
+        public static Matrix operator *(Matrix Operand1, Matrix Operand2)
+        {
+            if (Operand1.Columns != Operand2.Rows)
+                throw new System.Exception("The number of columns in the first Operand " +
+                  "must be equal to the number of rows in the second Operand");
+
+            Matrix result = new Matrix(Operand1.Rows, Operand2.Columns);
+
+            for (System.Int32 i = 0; i < result.Rows; i++)
+            {
+                for (System.Int32 j = 0; j < result.Columns; j++)
+                {
+                    result[i, j] = Multiply(Operand1, i, Operand2, j);
+                }
+            }
+
+            return result;
+        }
+        private static System.Double Multiply(Matrix Operand1, System.Int32 Row, Matrix Operand2, System.Int32 Column)
+        {
+            System.Double sum = 0;
+
+            for (System.Int32 i = 0; i < Operand1.Columns; i++)
+            {
+                sum += Operand1[Row, i] * Operand2[i, Column];
+            }
+
+            return sum;
+        }
     }
 }
