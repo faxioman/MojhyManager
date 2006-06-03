55055cb8827eae43e0d6d4f0589ffbc0548d3f1f
fabio <fabio@10b469d9-a807-0410-b80a-b41ed0d37aed>
2006-06-03 14:38:56 +0000

    git-svn-id: file:///Users/fabio/Desktop/svnrepos/MojhyManager@193 10b469d9-a807-0410-b80a-b41ed0d37aed

--- WebProject/MojhyUtils/DrawingExt.cs
+++ WebProject/MojhyUtils/DrawingExt.cs
@@ -1,13 +1,137 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
 
 namespace Mojhy.Utils.DrawingExt
 {
+    public class RectangleObject
+    {
+        private Rectangle l_rctInternalRectangle;
+        /// <summary>
+        /// Gets or sets the x-coordinate of the upper-left corner of this <see cref="T:PointObject"/>.
+        /// </summary>
+        /// <value>The x-coordinate of the upper-left corner.</value>
+        public int X
+        {
+            get { return l_rctInternalRectangle.X; }
+            set { l_rctInternalRectangle.X = value; }
+        }
+        /// <summary>
+        /// Gets or sets the y-coordinate of the upper-left corner of this <see cref="T:PointObject"/>.
+        /// </summary>
+        /// <value>The y-coordinate of the upper-left corner.</value>
+        public int Y
+        {
+            get { return l_rctInternalRectangle.Y; }
+            set { l_rctInternalRectangle.Y = value; }
+        }
+        /// <summary>
+        /// Gets or sets the width of this <see cref="T:PointObject"/>.
+        /// </summary>
+        /// <value>The width.</value>
+        public int Width
+        {
+            get { return l_rctInternalRectangle.Width; }
+            set { l_rctInternalRectangle.Width = value; }
+        }
+        /// <summary>
+        /// Gets or sets the height of this <see cref="T:PointObject"/>.
+        /// </summary>
+        /// <value>The height.</value>
+        public int Height
+        {
+            get { return l_rctInternalRectangle.Height; }
+            set { l_rctInternalRectangle.Height = value; }
+        }
+        /// <summary>
+        /// Gets the y-coordinate of the top edge of this <see cref="T:PointObject"/>.
+        /// </summary>
+        /// <value>The y-coordinate of the top edge as int.</value>
+        public int Top
+        {
+            get { return l_rctInternalRectangle.Top; }
+        }
+        /// <summary>
+        /// Gets the x-coordinate of the left edge of this <see cref="T:PointObject"/>.
+        /// </summary>
+        /// <value>The x-coordinate of the left edge as int.</value>
+        public int Left
+        {
+            get { return l_rctInternalRectangle.Left; }
+        }
+        /// <summary>
+        /// Gets the y-coordinate that is the sum of the Y and Height property values of this <see cref="T:PointObject"/>.
+        /// </summary>
+        /// <value>The y-coordinate of the bottom as int.</value>
+        public int Bottom
+        {
+            get { return l_rctInternalRectangle.Bottom; }
+        }
+        /// <summary>
+        /// Gets the x-coordinate that is the sum of X and Width property values of this <see cref="T:PointObject"/>.
+        /// </summary>
+        /// <value>The x-coordinate of the right as int.</value>
+        public int Right
+        {
+            get { return l_rctInternalRectangle.Right; }
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:RectangleObject"/> class.
+        /// </summary>
+        /// <param name="x">The x-coordinate of the upper-left corner of the rectangle.</param>
+        /// <param name="y">The y-coordinate of the upper-left corner of the rectangle.</param>
+        /// <param name="width">The width of the rectangle.</param>
+        /// <param name="height">The height of the rectangle.</param>
+        public RectangleObject(int x, int y, int width, int height){
+            l_rctInternalRectangle = new Rectangle(x, y, width, height);
+        }
+        public bool Contains (PointObject ptObj)
+        {
+            if ((ptObj.X < this.X) || (ptObj.X > this.Right) || (ptObj.Y < this.Y) || (ptObj.Y > this.Bottom))
+                return false;
+            else
+                return true;
+        }
+    }
+    /// <summary>
+    /// PointObject class
+    /// </summary>
+    public class PointObject
+    {
+        private Point l_ptInternalPoint;
+        /// <summary>
+        /// Gets or sets the x-coordinate of this <see cref="T:PointObject"/>.
+        /// </summary>
+        /// <value>The x-coordinate.</value>
+        public int X
+        {
+            get { return l_ptInternalPoint.X; }
+            set { l_ptInternalPoint.X = value; }
+        }
+        /// <summary>
+        /// Gets or sets the y-coordinate of this <see cref="T:PointObject"/>.
+        /// </summary>
+        /// <value>The y-coordinate.</value>
+        public int Y
+        {
+            get { return l_ptInternalPoint.Y; }
+            set { l_ptInternalPoint.Y = value; }
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:PointObject"/> class.
+        /// </summary>
+        /// <param name="X">The x-coordinate.</param>
+        /// <param name="Y">The y-coordinate.</param>
+        public PointObject(int X, int Y)
+        {
+            l_ptInternalPoint = new Point(X, Y);
+        }
+    }
     /// <summary>
     /// 3D PointF class
     /// </summary>
-    public struct Point3D
+    public class Point3D
     {
         Matrix  matrix;
         public Point3D(System.Int32 X, System.Int32 Y, System.Int32 Z)
