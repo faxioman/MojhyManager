7870c21b36c091156722318ae7c323a4e6ebee9a
fabio <fabio@10b469d9-a807-0410-b80a-b41ed0d37aed>
2006-10-01 07:20:08 +0000

    git-svn-id: file:///Users/fabio/Desktop/svnrepos/MojhyManager@233 10b469d9-a807-0410-b80a-b41ed0d37aed

---
+++ WebProject/MojhyUtils/Math.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mojhy.Utils
+{
+    class Math
+    {
+        /// <summary>
+        /// Calculate the direction angle from a source point to a destination point in RAD.
+        /// </summary>
+        /// <param name="px1">The PX1.</param>
+        /// <param name="py1">The py1.</param>
+        /// <param name="px2">The PX2.</param>
+        /// <param name="py2">The py2.</param>
+        /// <returns></returns>
+        public static double Angle(double px1, double py1, double px2, double py2)
+        {
+            // Negate X and Y value
+            double pxRes = px2 - px1;
+            double pyRes = py2 - py1;
+            double angle = 0.0;
+            // Calculate the angle
+            if (pxRes == 0.0)
+            {
+                if (pxRes == 0.0)
+                    angle = 0.0;
+                else if (pyRes > 0.0)
+                    angle = System.Math.PI / 2.0;
+                else
+                    angle = System.Math.PI * 3.0 / 2.0;
+            }
+            else if (pyRes == 0.0)
+            {
+                if (pxRes > 0.0)
+                    angle = 0.0;
+                else
+                    angle = System.Math.PI;
+            }
+            else
+            {
+                if (pxRes < 0.0)
+                    angle = System.Math.Atan(pyRes / pxRes) + System.Math.PI;
+                else if (pyRes < 0.0)
+                    angle = System.Math.Atan(pyRes / pxRes) + (2 * System.Math.PI);
+                else
+                    angle = System.Math.Atan(pyRes / pxRes);
+            }
+            // Convert to degrees
+            angle = angle * 180 / System.Math.PI;
+            //Return to RADIANT ;-) non chiedetemi il perchè 
+            angle = (((double)(360 - angle)) / 180) * Math.PI;
+            return angle;
+        }
+    }
+}
