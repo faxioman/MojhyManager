2da3a124d37906b23f2ec2b02cf3f5f61b3da187
fabio <fabio@10b469d9-a807-0410-b80a-b41ed0d37aed>
2006-11-25 11:36:07 +0000

    git-svn-id: file:///Users/fabio/Desktop/svnrepos/MojhyManager@281 10b469d9-a807-0410-b80a-b41ed0d37aed

--- WebProject/startXSP2.bat
+++ WebProject/startXSP2.bat
@@ -1,5 +1,5 @@
 
 @echo off
-call C:\Programmi\Mono-1.1.15\bin\setmonopath.bat
+call C:\Programmi\Mono-1.2\bin\setmonopath.bat
 cd /D C:\Mojhy\WebProject\Mojhy
 xsp2 --root . --port 8088 --applications /:.
